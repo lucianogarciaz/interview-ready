a34bb79a3a39400377408d0c92a14ddb44bc5223
Luciano Garcia Zeman <cluciano.gz@gmail.com>
2025-05-05 17:15:45 +0200

    add pre, post, inorder algorithms

--- inorder_traversal.go
+++ inorder_traversal.go
@@ -7,17 +7,50 @@ type BinaryTreeNode struct {
 	Right *BinaryTreeNode
 }
 
-func inorderTraversal(root *BinaryTreeNode) []int {
-	return recursive(root, []int{})
+func inorder(root *BinaryTreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	results := inorder(root.Left)
+	results = append(results, root.Val)
+	results = append(results, inorder(root.Right)...)
+	return results
 }
 
-// [4]
-func recursive(root *BinaryTreeNode, results []int) []int {
+//	{
+//		name: "Example 1: Simple tree",
+//		root: &BinaryTreeNode{
+//			Val: 1,
+//			Left: &BinaryTreeNode{
+//				Val: 2,
+//			},
+//			Right: &BinaryTreeNode{
+//				Val: 3,
+//			},
+//		},
+//		expected: []int{1, 2, 3},
+//	},
+//
+// [1 , 2 ,3 ]
+func preOrderTraversal(root *BinaryTreeNode) []int {
 	if root == nil {
-		return results
+		return []int{}
 	}
-	results = recursive(root.Left, results)
+
+	results := []int{root.Val} // [1, 2]
+	results = append(results, preOrderTraversal(root.Left)...)
+	results = append(results, preOrderTraversal(root.Right)...)
+
+	return results
+}
+
+func postOrderTraversal(root *BinaryTreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	results := postOrderTraversal(root.Left)
+	results = append(results, postOrderTraversal(root.Right)...)
 	results = append(results, root.Val)
-	results = recursive(root.Right, results)
+
 	return results
 }
--- inorder_traversal_test.go
+++ inorder_traversal_test.go
@@ -71,9 +71,157 @@ func TestInorderTraversal(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			result := inorderTraversal(tc.root)
+			result := inorder(tc.root)
+			// result := inorderTraversal(tc.root)
 			if !reflect.DeepEqual(result, tc.expected) {
 				t.Errorf("inorderTraversal(%v) = %v; want %v", tc.root, result, tc.expected)
+			}
+		})
+	}
+}
+func TestPreOrderTraversal(t *testing.T) {
+	testCases := []struct {
+		name     string
+		root     *BinaryTreeNode
+		expected []int
+	}{
+		{
+			name: "Example 1: Simple tree",
+			root: &BinaryTreeNode{
+				Val: 1,
+				Left: &BinaryTreeNode{
+					Val: 2,
+				},
+				Right: &BinaryTreeNode{
+					Val: 3,
+				},
+			},
+			expected: []int{1, 2, 3},
+		},
+		{
+			name: "Example 2: Complex tree",
+			root: &BinaryTreeNode{
+				Val: 1,
+				Left: &BinaryTreeNode{
+					Val: 2,
+					Left: &BinaryTreeNode{
+						Val: 4,
+					},
+					Right: &BinaryTreeNode{
+						Val: 5,
+						Left: &BinaryTreeNode{
+							Val: 6,
+						},
+						Right: &BinaryTreeNode{
+							Val: 7,
+						},
+					},
+				},
+				Right: &BinaryTreeNode{
+					Val: 3,
+					Right: &BinaryTreeNode{
+						Val: 8,
+						Left: &BinaryTreeNode{
+							Val: 9,
+						},
+					},
+				},
+			},
+			expected: []int{1, 2, 4, 5, 6, 7, 3, 8, 9},
+		},
+		{
+			name:     "Example 3: Empty tree",
+			root:     nil,
+			expected: []int{},
+		},
+		{
+			name: "Example 4: Single node",
+			root: &BinaryTreeNode{
+				Val: 1,
+			},
+			expected: []int{1},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := preOrderTraversal(tc.root)
+			if !reflect.DeepEqual(result, tc.expected) {
+				t.Errorf("preOrderTraversal(%v) = %v; want %v", tc.root, result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestPostOrderTraversal(t *testing.T) {
+	testCases := []struct {
+		name     string
+		root     *BinaryTreeNode
+		expected []int
+	}{
+		{
+			name: "Example 1: Simple tree",
+			root: &BinaryTreeNode{
+				Val: 1,
+				Left: &BinaryTreeNode{
+					Val: 2,
+				},
+				Right: &BinaryTreeNode{
+					Val: 3,
+				},
+			},
+			expected: []int{2, 3, 1},
+		},
+		{
+			name: "Example 2: Complex tree",
+			root: &BinaryTreeNode{
+				Val: 1,
+				Left: &BinaryTreeNode{
+					Val: 2,
+					Left: &BinaryTreeNode{
+						Val: 4,
+					},
+					Right: &BinaryTreeNode{
+						Val: 5,
+						Left: &BinaryTreeNode{
+							Val: 6,
+						},
+						Right: &BinaryTreeNode{
+							Val: 7,
+						},
+					},
+				},
+				Right: &BinaryTreeNode{
+					Val: 3,
+					Right: &BinaryTreeNode{
+						Val: 8,
+						Left: &BinaryTreeNode{
+							Val: 9,
+						},
+					},
+				},
+			},
+			expected: []int{4, 6, 7, 5, 2, 9, 8, 3, 1},
+		},
+		{
+			name:     "Example 3: Empty tree",
+			root:     nil,
+			expected: []int{},
+		},
+		{
+			name: "Example 4: Single node",
+			root: &BinaryTreeNode{
+				Val: 1,
+			},
+			expected: []int{1},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := postOrderTraversal(tc.root)
+			if !reflect.DeepEqual(result, tc.expected) {
+				t.Errorf("postOrderTraversal(%v) = %v; want %v", tc.root, result, tc.expected)
 			}
 		})
 	}
