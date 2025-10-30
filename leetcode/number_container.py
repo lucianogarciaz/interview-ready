987138b69b74743ec74bce3360f0689f669f3e7d
Luciano Garcia Zeman <cluciano.gz@gmail.com>
2025-10-30 21:13:26 +0100

    add number containers with heap

--- leetcode/number_container.py
+++ leetcode/number_container.py
@@ -68,3 +68,71 @@ class NumberContainers:
 # obj = NumberContainers()
 # obj.change(index,number)
 # param_2 = obj.find(number)
+
+
+class NumberContainersWithHeap:
+    """
+    [10] ->-1
+    2:10 ->[ 2:10 ]
+    1:10->[ 1:10, 2:10 ]
+    3:10->[ 1:10, 2:10, 3:10 ]
+    5:10 ->[ 1:10, 2:10, 3:10, 5:10 ]
+    10: [ 1:10, 2:10, 3:10, 5:10 ] -> 1
+    1:20->[ 1:20, 2:10, 3:10, 5:10 ]
+    
+
+    first try:
+    init:
+        map_index:{}
+        map_value:{}
+    
+    change():
+        2:10 ->map_index:{2:10}, map_value:{10:[2]}
+        1:10->map_index:{1:10, 2:10}, map_value:{10:[1,2]}
+        3:10->map_index:{1:10, 2:10, 3:10}, map_value:{10:[1,2,3]}
+        5:10 ->map_index:{1:10, 2:10, 3:10, 5:10}, map_value:{10:[1,2,3,5]}
+        10: map_value[10][0]
+        1:20->map_index:{1:10, 2:10, 3:10, 5:10}, map_value:{20:[1], 10:[2,3,5]}
+        if index not in map_index:
+            map_index[index] = number
+        else:
+            prev_number = map_index[index]
+            map_index[index] = number
+            # remove prev number
+            i = bisect.bisect_left(map_value[prev_number])
+            del map_value[prev_number][i]
+        
+        if number not in map_value:
+                map_value[number] = [index]
+        else
+            bisect.insort(map_value[number],index)
+
+    """
+    def __init__(self):
+        self.map_index = {}
+        self.map_value = {}
+
+    def change(self, index: int, number: int) -> None:
+        self.map_index[index] = number
+        if number not in self.map_value:
+            self.map_value[number] = []
+        heapq.heappush(self.map_value[number],index)
+
+
+    def find(self, number: int) -> int:
+        if number not in self.map_value:
+            return -1
+        heap = self.map_value[number]
+        while heap:
+            cand = heap[0]
+            if self.map_index.get(cand) == number:
+                return cand
+            heapq.heappop(heap)
+        return -1 
+        
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
